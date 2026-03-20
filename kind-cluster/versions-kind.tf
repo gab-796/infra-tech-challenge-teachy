13a6cc5297bfbc6a28cf56de62148a9153fed252
Gabriel <28798742+gab-796@users.noreply.github.com>
2026-03-20 19:20:29 -0300

    chore: state separated by folder

--- kind-cluster/versions-kind.tf
+++ kind-cluster/versions-kind.tf
@@ -2,8 +2,17 @@
 
 # Provider local para executar scripts
 terraform {
-  backend "local" {
-    path = "state/terraform.tfstate"
+  backend "s3" {
+    bucket                      = "tfstate"
+    key                         = "cluster/terraform.tfstate"
+    region                      = "us-east-1"
+    endpoints = {
+      s3 = "http://localhost:9100"
+    }
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
+    use_path_style              = true
   }
 
   required_providers {
